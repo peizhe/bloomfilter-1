ad7eb53ab34a29374ebe7c7e93b5ec7ec44a2e5c
stania (Won-gune Hwang) <stania.pe.kr@gmail.com>
2013-07-16 05:25:57 +0900

    added equals/hashCode in HashValue class

--- araqne-bloomfilter/src/main/java/org/araqne/bloomfilter/HashValue.java
+++ araqne-bloomfilter/src/main/java/org/araqne/bloomfilter/HashValue.java
@@ -2,12 +2,42 @@ package org.araqne.bloomfilter;
 
 public class HashValue<T> {
 
-	private int firstHashCode;
-	private int secondHashCode;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + firstHashCode;
+		result = prime * result + secondHashCode;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HashValue other = (HashValue) obj;
+		if (firstHashCode != other.firstHashCode)
+			return false;
+		if (secondHashCode != other.secondHashCode)
+			return false;
+		return true;
+	}
+
+	public int firstHashCode;
+	public int secondHashCode;
 
 	public HashValue(T key, HashFunction<T> firstFunction, HashFunction<T> secondFunction) {
 		firstHashCode = firstFunction.hashCode(key);
 		secondHashCode = secondFunction.hashCode(key);
+	}
+
+	public HashValue(int i, int j) {
+		firstHashCode = i;
+		secondHashCode = j;
 	}
 
 	public int getFirstHashCode() {
