e42a7e5038a4072d7cd97e27ed803a40af143ef4
xeraph <xeraph@eediom.com>
2013-11-10 22:47:54 +0900

    removed warnings

--- araqne-bloomfilter/src/main/java/org/araqne/bloomfilter/BloomFilter2.java
+++ araqne-bloomfilter/src/main/java/org/araqne/bloomfilter/BloomFilter2.java
@@ -16,7 +16,6 @@
 package org.araqne.bloomfilter;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
